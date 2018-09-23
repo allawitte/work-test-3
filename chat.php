c999961416aace3cf39ddcf6950c2209e900eafa
= <alla_88@inbox.lv>
2018-09-23 09:14:37 -0300

    added mysql insert handler

---
+++ chat.php
@@ -0,0 +1,20 @@
+<?php
+include("db.php");
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+$author = $request->author;
+$message = $request->message;
+$time = $request->time;
+$q = mysql_query("INSERT INTO chats (author, message, time) VALUES ('$author', '$message', '$time')");
+if ($q) === TRUE) {
+    echo "ok";
+} else {
+    echo "error";
+}
+
+
+//var data = {
+//            author: author,
+  //          message: vm.newMsg,
+//            time: new Date().getTime()
+ //       };
--- js/chat.js
+++ js/chat.js
@@ -1,8 +1,17 @@
 app.controller('ChatController', ChatController);
 function ChatController($http, $state) {
     var vm = this;
-    console.log('$state', $state.getCurrentPath()[1].paramValues.login);
+    var author = $state.getCurrentPath()[1].paramValues.login;
     vm.send = function(){
+        var data = {
+            author: author,
+            message: vm.newMsg,
+            time: new Date().getTime()
+        };
+        $http.post('chat.php', data)
+            .then(function(res){
+                console.log(res);
+            });
         console.log('chat controller', vm);
     }
 
